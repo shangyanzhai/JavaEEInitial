c066741a5a5c1b5160124eafc9a7e8bd53f80aaf
殇雁斋 <2091163852@qq.com>
2023-09-11 01:01:01 +0800

    9_11

---
+++ src/JavaEE/Threads/ThreadCommunication/ThreadSafe/Lock/锁的策略/UseRWLock.java
@@ -0,0 +1,21 @@
+package JavaEE.Threads.ThreadCommunication.ThreadSafe.Lock.锁的策略;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class UseRWLock {
+
+    public static void main(String[] args) {
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        Lock readLock = readWriteLock.readLock();
+        Lock writeLock = readWriteLock.writeLock();
+
+        writeLock.lock();    // 主线程先加读锁
+
+        new Thread(() -> {
+            writeLock.lock();
+            System.out.println("子线程加锁成功");
+        }).start();
+    }
+}
